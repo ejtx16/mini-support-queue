5297d5324eaf9a15b030c7223394565e529fba5b
James Tomes Platino <platinoearljames@gmail.com>
2026-02-07 17:42:11 +0800

    chore: update main.tsx MSW env

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,9 @@ async function enableMocking() {
   const { worker } = await import('./mocks/browser')
   return worker.start({
     onUnhandledRequest: 'bypass',
+    serviceWorker: {
+      url: import.meta.env.BASE_URL + 'mockServiceWorker.js',
+    },
   })
 }
 
